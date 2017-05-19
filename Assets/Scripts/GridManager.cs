88804e05f8bc213293725bdd0821b69363253f0f
vtaipale <valtter.taipale@gmail.com>
2017-05-20 00:15:06 +0300

    Grid Change Works v1
    For now Grids can be changed (to LowTrences). Now to implement: How to
    change to different Grid Types!

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -15,13 +15,12 @@ public class GridManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		for (int forX = 0; forX <= ScaleX; forX = forX +2)
+
+		for (int forX = 0; forX <= ScaleX*2; forX = forX +2)
 		{
-			for (int forY = 0; forY <= ScaleY; forY = forY +2)
+			for (int forY = 0; forY <= ScaleY*2; forY = forY +2)
 			{
-				GameObject GRID_PIECE = (GameObject)Instantiate (GRIDTEMPLATE, (this.transform.position + new Vector3(forX, 0, forY)), this.transform.rotation);
-				GRID_PIECE.transform.parent = this.transform;
-				Grid GRID_PIECE_GRIDDITY = GRID_PIECE.GetComponent<Grid> ();
+				Grid GRID_PIECE_GRIDDITY = this.CreateGrid (Ground, (this.transform.position + new Vector3 (forX, 0, forY)));
 
 				GRID_PIECE_GRIDDITY.x = forX;
 				GRID_PIECE_GRIDDITY.y = forY;
@@ -29,7 +28,43 @@ public class GridManager : MonoBehaviour {
 		}
 
 	}
-	
+
+	public void CreateGrid (GameObject TypeToChange, Vector3 NewGridLocation, Grid OldGrid)
+	{
+
+		Grid NewGrid = this.CreateGrid (TypeToChange, NewGridLocation);
+
+		NewGrid.x = OldGrid.x;
+		NewGrid.y = OldGrid.y;
+
+		NewGrid.UpdateName ();
+
+	}
+
+	public Grid CreateGrid (GameObject TypeToChange, Vector3 NewGridLocation)
+	{
+		GameObject GRID_PIECE = (GameObject)Instantiate (GRIDTEMPLATE, NewGridLocation, this.transform.rotation);
+		GRID_PIECE.transform.parent = this.transform;
+
+		Grid GRID_PIECE_GRIDDITY = GRID_PIECE.GetComponent<Grid> ();
+
+		//GRID_PIECE_GRIDDITY.gameObject.AddComponent(TypeToChange) as GameObject;
+
+	//	GameObject NewLook = GRID_PIECE_GRIDDITY.gameObject.AddComponent("GameObject") as GameObject;
+
+		GRID_PIECE_GRIDDITY.UseThisModel (TypeToChange);
+
+		//GameObject NewLook = GRID_PIECE.AddComponent<GameObject>();
+
+
+
+
+		//GRID_PIECE_GRIDDITY.ChangeTo (Type);
+
+
+		return GRID_PIECE_GRIDDITY;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
