92c2e6f8c50780de2a03fbdf5d11ee3b191c5575
vtaipale <valtter.taipale@gmail.com>
2017-05-19 21:37:16 +0300

    Basic Grid
    Gridmanager creates lots of Grids who manage themselves.
    
    To implement: Grid change according to imput!

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour {
+
+	public int ScaleX = 10;
+	public int ScaleY = 10;
+
+	public GameObject GRIDTEMPLATE;
+
+	public GameObject Ground;
+	public GameObject Trench_Low;
+	public GameObject Trench_Deep;
+
+	// Use this for initialization
+	void Start () {
+		for (int forX = 0; forX <= ScaleX; forX++)
+		{
+			for (int forY = 0; forY <= ScaleY; forY++)
+			{
+				GameObject GRID_PIECE = (GameObject)Instantiate (GRIDTEMPLATE, (this.transform.position + new Vector3(forX, 0, forY)), this.transform.rotation);
+				GRID_PIECE.transform.parent = this.transform;
+				Grid GRID_PIECE_GRIDDITY = GRID_PIECE.GetComponent<Grid> ();
+
+				GRID_PIECE_GRIDDITY.x = forX;
+				GRID_PIECE_GRIDDITY.y = forY;
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+
+
+}
