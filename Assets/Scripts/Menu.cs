ebc2f627465cdb8247560e2346718dd80ec997c9
GameJam <GameJam@Niko-MacBook-Air.local>
2017-05-20 19:03:32 +0300

    menuscene added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Menu : MonoBehaviour {
+	private NetworkManager manager;
+	// Use this for initialization
+	void Start () {
+		manager = GetComponent<NetworkManager>();
+	}
+
+	public void StartServer()
+	{
+		manager.StartHost();
+	}
+
+	public void JoinServer()
+	{
+		if (manager.networkAddress.Trim() == "")
+			manager.networkAddress = "localhost";
+		manager.StartClient();
+	}
+
+	public void SetServerIp(string serverIp)
+	{
+		manager.networkAddress = serverIp;
+	}
+
+
+}
