5c81a5cdef92a500fb758ec27d39270af46add09
vtaipale <valtter.taipale@gmail.com>
2017-05-20 14:14:14 +0300

    Multiplayer sync fix 1
    Basically moved every important function to Cmd (aka server) side

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -85,9 +85,9 @@ public class PlayerMove : NetworkBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
       if (Input.mouseScrollDelta.y < 0) {
-    		this.CycleWhatToBuild(1);
+    		this.CmdCycleWhatToBuild(1);
       } else if (Input.mouseScrollDelta.y > 0) {
-        this.CycleWhatToBuild(-1);
+        this.CmdCycleWhatToBuild(-1);
       }
 
         mouse.LookRotation(transform, cam.transform);
@@ -121,17 +121,6 @@ public class PlayerMove : NetworkBehaviour {
         gravVelocity = jumpVelocity;
     }
 
-	///Cycles what can be build now. Short list for now! (once needs certain equipments??)
-	/// SHOULD THIS BE COMMAND TO SERveR?? (soo it looks to all the same??)
-	void CycleWhatToBuild(int dir)
-	{
-    Debug.Log(WhatToBuild);
-		this.PlaySoundHere (ItemSwichSound);
-    tools[WhatToBuild].SetActive(false);
-    WhatToBuild += dir;
-    WhatToBuild = mod(WhatToBuild, tools.Length);
-    tools[WhatToBuild].SetActive(true);
-	}
 
   int mod(int x, int m) {
       int r = x%m;
@@ -142,33 +131,37 @@ public class PlayerMove : NetworkBehaviour {
 	/// Plays single sound and kills itself. Note: allows multiple at the same time!!
 	/// </summary>
 	/// <param name="WhatToPlay">What to play.</param>
-	void PlaySoundHere(AudioClip WhatToPlay)
-	{
-		GameObject Soundie = (GameObject)Instantiate (SoundplayerPrefab, this.transform.position, this.transform.rotation);
 
-		AudioSource SoundieSound = Soundie.GetComponent<AudioSource> ();
-		SoundieSound.clip = WhatToPlay;
-
-	}
 
     // [Command] tells this will be called from client but invoked on server
     // Cmd-prefix in Command-methods is a common practice
     [Command]
     void CmdFire(Vector3 dir) {
-        RpcShoot(dir);
+        CmdShoot(dir);
     }
 
-    [ClientRpc]
-    void RpcShoot(Vector3 dir)
-    {
-        RaycastHit hit;
+	///Cycles what can be build now. Short list for now! (once needs certain equipments??)
+	void CmdCycleWhatToBuild(int dir)
+	{
+		Debug.Log(WhatToBuild);
+		this.CmdPlaySoundHere (ItemSwichSound);
+		tools[WhatToBuild].SetActive(false);
+		WhatToBuild += dir;
+		WhatToBuild = mod(WhatToBuild, tools.Length);
+		tools[WhatToBuild].SetActive(true);
+	}
 
-        Debug.Log(toolActions[WhatToBuild]);
+
+	void CmdShoot(Vector3 dir)
+	{
+		RaycastHit hit;
+
+		Debug.Log(toolActions[WhatToBuild]);
 		if (toolActions[WhatToBuild] == "Rifle") {
 
 			// TODO: ACTUAL SHOOTINGS!
 
-			this.PlaySoundHere (RifleShootSound);
+			this.CmdPlaySoundHere (RifleShootSound);
 
 		}
 		else if (Physics.Raycast (transform.position, dir, out hit, 4)) {
@@ -186,7 +179,7 @@ public class PlayerMove : NetworkBehaviour {
 				//Pickaxe GOES DOWN
 				if (toolActions[WhatToBuild] == "PickAxe") {
 
-					this.PlaySoundHere (PickAxeDigSound);
+					this.CmdPlaySoundHere (PickAxeDigSound);
 
 					if (GridThatWasHit.WhatIam == GridThatWasHit.Mother.Trench_Low.name)
 						GridThatWasHit.ChangeTo (GridThatWasHit.Mother.Trench_Deep);
@@ -200,7 +193,7 @@ public class PlayerMove : NetworkBehaviour {
 				//Shovel goes  UP
 				else if (toolActions[WhatToBuild] == "Shovel") {
 
-					this.PlaySoundHere (ShovelDigSound);
+					this.CmdPlaySoundHere (ShovelDigSound);
 
 					if (GridThatWasHit.WhatIam == GridThatWasHit.Mother.Trench_Low.name)
 						GridThatWasHit.ChangeTo (GridThatWasHit.Mother.Ground);
@@ -212,5 +205,29 @@ public class PlayerMove : NetworkBehaviour {
 
 			}
 		}
-    }
+	}
+
+	void CmdPlaySoundHere(AudioClip WhatToPlay)
+	{
+		GameObject Soundie = (GameObject)Instantiate (SoundplayerPrefab, this.transform.position, this.transform.rotation);
+
+		AudioSource SoundieSound = Soundie.GetComponent<AudioSource> ();
+		SoundieSound.clip = WhatToPlay;
+
+	}
+//
+//    [ClientRpc]
+//
+//	/// <summary>
+//	/// Plays single sound and kills itself. Note: allows multiple at the same time!!
+//	/// </summary>
+//	/// <param name="WhatToPlay">What to play.</param>
+//	void RpcPlaySoundHere(AudioClip WhatToPlay)
+//	{
+//		GameObject Soundie = (GameObject)Instantiate (SoundplayerPrefab, this.transform.position, this.transform.rotation);
+//
+//		AudioSource SoundieSound = Soundie.GetComponent<AudioSource> ();
+//		SoundieSound.clip = WhatToPlay;
+//
+//	}
 }
