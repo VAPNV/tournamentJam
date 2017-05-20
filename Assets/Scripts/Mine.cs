918fbdba1cfb5f57fb610e220f36dc378e15da1e
Wadelma <delma@del.ma>
2017-05-21 01:04:56 +0300

    Start of mine

---
+++ Assets/Scripts/Mine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour {
+	public Combat owner;
+	public int damage;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, 1);
+		foreach (Collider c in hitColliders) {
+			var hit = c.gameObject;
+			if (hit != gameObject) {
+				var hitCombat = hit.GetComponent<Combat>();
+				if (hitCombat != null) {
+					hitCombat.TakeDamage(damage, owner);
+					Destroy(gameObject);
+				}
+			}
+		}
+	}
+}
