3436833c95aa5fba48d89c73c18243846c40498f
GameJam <GameJam@Niko-MacBook-Air.local>
2017-05-21 14:13:38 +0300

    HUD update with a simple script

---
+++ Assets/Scripts/Hud.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Hud : MonoBehaviour
+{
+	private int blue_score = 0;
+	private int orange_score = 0;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GameObject.Find ("BlueScore").GetComponent<Text> ().text = ""+blue_score;
+		GameObject.Find ("OrangeScore").GetComponent<Text> ().text = ""+orange_score;
+	}
+
+	void SetScore(int orange, int blue) {
+		blue_score = blue_score;
+		orange_score = blue; 
+	}
+}
+
